bd266e435a02d32908abaaa85d5ba13602bb419f
HarshaSandaruvan <133190578+HarshaSandaruvan@users.noreply.github.com>
2023-12-24 14:53:15 +0530

    editor component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import './App.css';
+import React, { useState } from 'react'
+import Editor from './components/Editor.js';
 
 function App() {
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [js, setJs] = useState('')
   return (
-    <div className="App">
-      hello
-    </div>
+    <>
+      <div className='pane top-pane'>
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="js"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+
+
+
+      </div>
+      <div className='pane'>
+        <iframe
+          title='output'
+          sandbox='allow-scripts'
+          frameBorder="0"
+          width='100%'
+          height='100%'
+        />
+
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Editor.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/css/css'
+import { Controlled as ControlledEditor } from 'react-codemirror2'
+
+export default function Editor(props) {
+    const {
+        language,
+        displayName,
+        value,
+        onChange,
+    } = props
+
+    function handleChange(editor, data, value) {
+        onChange(value)
+    }
+
+    return (
+        <div className='editor-container'>
+            <div className='editor-title'>{displayName}
+                <button>c/o</button>
+            </div>
+            <ControlledEditor
+                onBeforeChange={handleChange}
+                value={value}
+                className='code-mirror-warapper'
+                options={{
+                    lineWrapper: true,
+                    lint: true,
+                    mode: language,
+                    lineNumbers: true,
+                    theme: 'material'
+                }}
+            />
+        </div>
+    )
+}
